a07e447441b5ce79a755674f55b3fc67ba9e8ff0
santiago morales <santiagomoraless2001@gmail.com>
2022-09-15 15:17:03 -0500

    navbar add

--- src/app/components/home/users.component.ts
+++ src/app/components/home/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(
+    private router : Router
+  ) { }
 
   ngOnInit(): void {
   }
-
+  closesecion(){
+    localStorage.removeItem('token')
+    this.router.navigate(['login'])
+  }
+  
 }
